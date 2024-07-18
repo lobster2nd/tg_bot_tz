8ee47bfbe2eae16962ca5a3229804223a11b71d2
lobst <lobster2@mail.ru>
2024-07-18 20:06:01 +0300

    Добавлена оплата в тестовом режиме

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,9 @@
+import os
+
 from aiogram import F, Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
+from aiogram.types.message import ContentType
 
 from app.google_sheets.google_sheets import get_cell_value, write_cell_value
 from app.maps.maps import generate_location_link
@@ -12,6 +15,10 @@ import app.keyboards as kb
 load_dotenv()
 
 router = Router()
+
+PRICES = [
+    LabeledPrice(label='Hello', amount=200),
+]
 
 
 @router.message(CommandStart())
@@ -37,6 +44,36 @@ async def get_cell(message: Message, cell_value='A2'):
 @router.message(F.text == 'Карта')
 async def get_location(message: Message, place='улица Ленина 1, Москва'):
     await message.answer(generate_location_link(place))
+
+
+@router.message(F.text == 'Покупка')
+async def buy_process(message: Message):
+    await message.answer_invoice(
+                           title='Тестовая покупка',
+                           description='Описание тестовой покупки',
+                           provider_token=os.getenv('PAYMENTS_TOKEN'),
+                           currency='USD',
+                           photo_url='https://quod.lib.umich.edu/f/fc/images/13761232.0043.102-00000001.jpg',
+                           photo_height=512,
+                           photo_width=512,
+                           photo_size=512,
+                           need_email=False,
+                           need_phone_number=False,
+                           is_flexible=False,
+                           prices=PRICES,
+                           start_parameter='example',
+                           payload='invoice',
+                           need_shipping_address=False)
+
+
+@router.pre_checkout_query(lambda q: True)
+async def checkout_process(pre_checkout_query: PreCheckoutQuery):
+    await pre_checkout_query.answer(ok=True)
+
+
+@router.message()
+async def successful_payment(message: Message):
+    await message.answer(f'Оплата прошла успешно')
 
 
 @router.message(F.text)
--- app/keyboards.py
+++ app/keyboards.py
@@ -2,6 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Изображение')],
-    [KeyboardButton(text='Ячейка А2'), KeyboardButton(text='Карта')]
+    [KeyboardButton(text='Ячейка А2'), KeyboardButton(text='Карта')],
+    [KeyboardButton(text='Покупка')]
 ],      resize_keyboard=True,
         input_field_placeholder='Введите дату DD.MM.YYYY')
